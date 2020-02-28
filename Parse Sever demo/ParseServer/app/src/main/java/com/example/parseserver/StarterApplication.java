887d485b8fc4b7db267d70b47c450a21396e8927
zexi yu <eezxyu@gmail.com>
2020-02-28 13:00:12 +0800

    Write some advanced queries by adding constrains.

--- Parse Sever demo/ParseServer/app/src/main/java/com/example/parseserver/StarterApplication.java
+++ Parse Sever demo/ParseServer/app/src/main/java/com/example/parseserver/StarterApplication.java
@@ -11,6 +11,7 @@ package com.example.parseserver;
 import android.app.Application;
 import android.util.Log;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.Parse;
 import com.parse.ParseACL;
@@ -20,55 +21,74 @@ import com.parse.ParseQuery;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 
+import java.util.List;
+
 
 public class StarterApplication extends Application {
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
+    @Override
+    public void onCreate() {
+        super.onCreate();
 
-    // Enable Local Datastore.
-    Parse.enableLocalDatastore(this);
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
 
-    // Add your initialization code here
-    Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-            .applicationId("4dc14be04cd16942fdda3cc074c11d02eef500d1")
-            .clientKey("d5ee1d253722b75c7635cfccc9f9ad5d2a02617f")
-            .server("http://18.217.23.166:80/parse/")
-            .build()
-    );
+        // Add your initialization code here
+        Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+                .applicationId("4dc14be04cd16942fdda3cc074c11d02eef500d1")
+                .clientKey("d5ee1d253722b75c7635cfccc9f9ad5d2a02617f")
+                .server("http://18.217.23.166:80/parse/")
+                .build()
+        );
 
-    // save object
-    ParseObject twitter = new ParseObject("Twitter");
-    twitter.put("username", "Zoe");
-    twitter.put("tweet", "I want to go outside of Wuhan, please!");
-    twitter.saveInBackground(new SaveCallback() {
-      @Override
-      public void done(ParseException e) {
-        if(e == null) {
-          Log.i("twitter", "succeed!");
-        } else {
-          Log.i("twitter", "failed");
-        }
-      }
-    });
+        // save object
+//        ParseObject twitter = new ParseObject("Twitter");
+//        twitter.put("username", "Zoe");
+//        twitter.put("tweet", "Youhe bread is so yummy!");
+//        twitter.saveInBackground(new SaveCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e == null) {
+//                    Log.i("twitter", "succeed!");
+//                } else {
+//                    Log.i("twitter", "failed");
+//                }
+//            }
+//        });
 
-    // retrieve object
-    ParseQuery<ParseObject> query = ParseQuery.getQuery("Twitter");
-    query.getInBackground("yqKuR4nmbn", new GetCallback<ParseObject>() {
-      @Override
-      public void done(ParseObject object, ParseException e) {
-        if(e == null) {
-          // update and retrieve
-          object.put("tweet", "I want to come back to the U.S ASAP");
-          object.saveInBackground();
-          Log.i("object username", object.getString("username"));
-          Log.i("object tweet", object.getString("tweet"));
-        } else {
-          Log.i("retrieve failed", e.toString());
-        }
-      }
-    });
+        // retrieve object
+//        ParseQuery<ParseObject> query = ParseQuery.getQuery("Twitter");
+//        query.getInBackground("yqKuR4nmbn", new GetCallback<ParseObject>() {
+//            @Override
+//            public void done(ParseObject object, ParseException e) {
+//                if(e == null) {
+//                    // update and retrieve
+//                    object.put("tweet", "I want to come back to the U.S ASAP");
+//                    object.saveInBackground();
+//                    Log.i("object username", object.getString("username"));
+//                    Log.i("object tweet", object.getString("tweet"));
+//                } else {
+//                    Log.i("retrieve failed", e.toString());
+//                }
+//            }
+//        });
+
+        /* advance query */
+        // loop through the objects in Class Twitter
+        ParseQuery<ParseObject>  query = ParseQuery.getQuery("Twitter");
+        // set some constrains to the query
+        query.whereEqualTo("username", "Blama");
+        query.setLimit(1);
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                for(ParseObject object : objects) {
+                    if(e == null) {
+                        Log.i("objects in Twitter",  object.getString("tweet"));
+                    }
+                }
+            }
+        });
 
 
 //    ParseUser.enableAutomaticUser();
@@ -78,5 +98,5 @@ public class StarterApplication extends Application {
 //    defaultACL.setPublicWriteAccess(true);
 //    ParseACL.setDefaultACL(defaultACL, true);
 
-  }
+    }
 }
