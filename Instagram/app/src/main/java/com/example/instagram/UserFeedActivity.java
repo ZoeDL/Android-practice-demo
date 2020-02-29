c49826e0845e02f445d72813bb0596a8c69f213a
zexi yu <eezxyu@gmail.com>
2020-02-29 23:31:39 +0800

    Query all the images from the server and convert them from byte array to bitmap.

--- Instagram/app/src/main/java/com/example/instagram/UserFeedActivity.java
+++ Instagram/app/src/main/java/com/example/instagram/UserFeedActivity.java
@@ -3,12 +3,22 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.parse.FindCallback;
+import com.parse.GetDataCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
+
+import java.util.List;
 
 public class UserFeedActivity extends AppCompatActivity {
 
@@ -21,15 +31,42 @@ public class UserFeedActivity extends AppCompatActivity {
         String activeUser = intent.getStringExtra("username");
         setTitle(activeUser + "'s feed");
 
-        LinearLayout feedsLayout = findViewById(R.id.feedsLayout);
-        ImageView imageView = new ImageView(getApplicationContext());
-        imageView.setLayoutParams(new ViewGroup.LayoutParams(
-                ViewGroup.LayoutParams.MATCH_PARENT,
-                ViewGroup.LayoutParams.WRAP_CONTENT
-        ));
-        // temporary set the image
-        imageView.setImageDrawable(getResources().getDrawable(R.drawable.instagram));
+        final LinearLayout feedsLayout = findViewById(R.id.feedsLayout);
 
-        feedsLayout.addView(imageView);
+        // query all images uploaded by the active user
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Image");
+        query.whereEqualTo("username", activeUser);
+        query.orderByDescending("createdAt");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null) {
+                    for(ParseObject object : objects) {
+                        ParseFile file = (ParseFile) object.get("image");
+                        file.getDataInBackground(new GetDataCallback() {
+                            @Override
+                            public void done(byte[] data, ParseException e) {
+                                if(e == null && data != null) {
+                                    Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                                    ImageView imageView = new ImageView(getApplicationContext());
+                                    imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                                            ViewGroup.LayoutParams.MATCH_PARENT,
+                                            ViewGroup.LayoutParams.WRAP_CONTENT
+                                    ));
+                                    // temporary set the image
+                                    imageView.setImageBitmap(bitmap);
+                                    feedsLayout.addView(imageView);
+                                }
+                            }
+                        });
+                    }
+                } else {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
+
     }
 }
