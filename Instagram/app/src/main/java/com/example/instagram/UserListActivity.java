f29bfa4d72e489babea17a86fb6325a31f947912
zexi yu <eezxyu@gmail.com>
2020-02-29 16:50:04 +0800

    Build a menu for sharing photos. Let user import photos from the camera. 1. Create a menu resource file and put it in the new directory called menu under the resources directory. 2. Ask users' permssion for accessing the photos. If permissioin is granted, start the activity for picking photos from the media.

--- Instagram/app/src/main/java/com/example/instagram/UserListActivity.java
+++ Instagram/app/src/main/java/com/example/instagram/UserListActivity.java
@@ -1,9 +1,21 @@
 package com.example.instagram;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -12,6 +24,8 @@ import com.parse.ParseException;
 import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -40,8 +54,55 @@ public class UserListActivity extends AppCompatActivity {
                 userList.setAdapter(arrayAdapter);
             }
         });
+    }
 
+    // build a menu for sharing photos
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.share_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.share) {
+            if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            } else {
+                getPhoto();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == 1) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getPhoto();
+            }
+        }
+    }
+
+    public void getPhoto() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri image = data.getData();
+            try {
+                MediaStore.Images.Media.getBitmap(this.getContentResolver(),image);
+                Log.i("!!!!!!!", "photo received!");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
