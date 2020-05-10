4fb8a1099e8707140adf26f4862dabe017a578c5
Zoeyyy0201 <32622217+ZoeDL@users.noreply.github.com>
2020-05-10 17:33:07 +0800

    Update StarterApplication.java

--- Instagram/app/src/main/java/com/example/instagram/StarterApplication.java
+++ Instagram/app/src/main/java/com/example/instagram/StarterApplication.java
@@ -14,9 +14,9 @@ public class StarterApplication extends Application {
 
         // Add initialization code to connect to the Parse Server
         Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
-                .applicationId("4dc14be04cd16942fdda3cc074c11d02eef500d1")
-                .clientKey("d5ee1d253722b75c7635cfccc9f9ad5d2a02617f")
-                .server("http://18.217.23.166:80/parse/")
+                .applicationId("XXXXXXXXXXXXXXX")
+                .clientKey("XXXXXXXXXXXXXXXXXXX")
+                .server("XXXXXXXXXXXXXXXXXXXX")
                 .build()
         );
 
